81ab0620da53329a39b88b2df817e5bfbc5b9888
Ravindra Bandara <ravindra.bandara@gmail.com>
2017-03-23 18:38:19 -0400

    initial commit

--- test.js
+++
@@ -1,9 +0,0 @@
-const Code = require('code'); // assertion library
-const Lab = require('lab');
-const lab = exports.lab = Lab.script();
-
-lab.test('returns true when 1 + 1 equals 2', (done) => {
-
-    Code.expect(1 + 1).to.equal(2);
-    done();
-});
